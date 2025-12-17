ff51e8781e5f602507ce3ee18b34ec4151dad91f
codebuilder2018 <code.builder2018@gmail.com>
2025-12-17 17:29:57 +0530

    2-ui-shadcn-themes

--- app/page.js
+++ app/page.js
@@ -1,7 +1,14 @@
+'use client'
+import { Button } from "@/components/ui/button"
+import { useTheme } from "next-themes"
+
 export default function Home() {
+  const {setTheme} = useTheme()
   return (
     <div>
-      Hi! Every one...
+      <h2>Hi! Every one...</h2>
+      <Button onClick={()=> setTheme('dark')}>Dark Theme</Button>
+      <Button onClick={()=> setTheme('light')}>Light Theme</Button>
     </div>
   );
 }
